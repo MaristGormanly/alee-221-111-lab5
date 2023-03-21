81516ee1bd30efcd92ce43809b0813c507e9f342
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-03-21 14:14:39 -0400

    signup

---
+++ client/public/js/signup.js
@@ -0,0 +1,6 @@
+/* Takes to 'sign up successful' page after signing up */
+document.getElementById("signUp").addEventListener("click", redirectFunction);
+function redirectFunction() {
+    window.location.href = "#";
+}
+
