3918f2c12029f88b3ff901cf634b043199b8b775
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-03-21 14:15:32 -0400

    app

--- server/app.js
+++ server/app.js
@@ -5,4 +5,14 @@ app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views'})
 })
 
-app.listen(1337, () => console.log('Marist Chatter listening on port 1337!'));
+app.get('/signup', function (req, res) {
+    res.sendFile('signup.html', {root: './client/views'})
+})
+
+app.get('/home', function (req, res) {
+    res.sendFile('home.html', {root: './client/views'})
+})
+
+app.use(express.static('client/public'));
+
+app.listen(1337, () => console.log('Social Media Login Page is now up!'));
