8355067a96e0fc294278019cea1f210d5c5d34ee
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-04-15 22:46:38 -0400

    user

---
+++ server/model/user.js
@@ -0,0 +1,12 @@
+function User(firstName, lastName, email, username, password) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.username = username;
+    this.password = password;
+}
+
+exports.createUser = function(firstName, lastName, email, username, password) {
+    return new User(firstName, lastName, email, username, password);
+}
+console.log("[user] initialized");
