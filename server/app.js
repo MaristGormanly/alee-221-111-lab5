53a20e88e2d93b70e03c1f06f2bf998e4a2d436c
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-04-15 22:44:01 -0400

    server side

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json({type: 'application/json'}));
+
+var users = require('./controller/userController');
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views'})
@@ -14,5 +18,20 @@ app.get('/home', function (req, res) {
 })
 
 app.use(express.static('client/public'));
+
+app.route('/api/users')
+    .get(users.getAllUsers)
+    .post(users.saveUser)
+
+app.route('/api/users/:userId')
+    .get(users.getUser)
+    .delete(users.deleteUser)
+    .patch(users.updateUser)
+    .put(users.completeUpdateUser)
+
+let userRoute = require('./route/userRoute');
+app.use('/api/user', userRoute);
+let userController = require('./controller/userController');
+let userService = require('./service/userService');
 
 app.listen(1337, () => console.log('Social Media Login Page is now up!'));
