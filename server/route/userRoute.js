45d3ef6dede2e623e2ccf5f2dcbfe825acf4d741
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-04-15 22:45:51 -0400

    route

---
+++ server/route/userRoute.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+let userController = require('../controller/userController');
+
+console.log("[userRoute] initialized");
+
+router.route('/')
+    .get((req, res) => {
+        userController.getAllUsers(req, res);
+    })
+    .post((req, res) => {
+        userController.saveUser(req, res);
+    })
+
+router.route('/:index')
+    .get((req, res) => {
+        userController.getUser(req, res);
+    })
+
+module.exports = router;
