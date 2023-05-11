9ccf998842000e944b3c6ffd0db08aab30487a99
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-05-10 23:47:54 -0400

    index

--- client/public/js/index.js
+++ client/public/js/index.js
@@ -1,18 +1,74 @@
+ function getAllUsers() {
+  fetch('/api/user').then(function(response) {
+      if (response.status !== 200) {
+          console.log('problem with ajax call! ' + response.status + " msg: " + response.value);
+          return;
+      }
+      response.json().then(function(data) {  
+      var userHtml ="<ol>";
+          for(i in data) {
+            userHtml += "<li id='" + data[i].id + "'>" + data[i].email + "</li>";
+          }  
+    userHtml += "</ol>";
+    document.getElementById('users').innerHTML = userHtml;
+      });
+  });
+} 
+
+function logIn(userId) {
+	// set the cookie!
+	var now = new Date();
+	var EXPIRE_TIME = 10000; // 10 seconds
+	now.setTime(now.getTime() + EXPIRE_TIME);
+	document.cookie = "userId=" + userId +";expires=" 
+		+ now.toUTCString() + ";";
+    window.location='http://localhost:1337/home';
+}
+
+function userClick() {
+	var targetElement = event.target || event.srcElement;
+	logIn(targetElement.id);
+}
+
+var usersEl = document.getElementById('users');
+usersEl.addEventListener("click", userClick)
+
+getAllUsers();
+
 /* Collects login information, */
-function validateForm() {
+/* document.getElementById("myFrame").addEventListener("load", loadFunction);
+  function loadFunction() {
+    fetch('/api/user').then(function(response) {
+      if (response.status !== 200) {
+        console.log('problem with ajax call! ' + response.status + " msg: " + response.value);
+        return;
+      }
+      response.json().then(function(data) {
+        var userHtml = "<ol>";
+        for (i in data) {
+          userHtml += "<li id='" + data[i].email + "</li>";
+        }
+        userHtml += "</ol>";
+        document.getElementById("user").innerHTML = userHtml;
+      });
+    });
+    document.getElementById("demo").innerHTML = "Website loaded.";
+  } */
+
+/* function validateForm() {
   let x = document.forms["loginForm"]["user"]["password"].value;
   if (x == "") {
     console.log("Please fill out this field.");
   }
 }
 
-/* Hit 'log in' and goes to next page */
-document.getElementById("login").addEventListener("click", redirectFunction);
-    function redirectFunction() {
-        window.location.href = "http://localhost:1337/home";
-    }
+/* Hit 'log in' and goes to next page 
+document.getElementById("loginbutton").addEventListener("click", redirectFunction);
+  function redirectFunction() {
+    window.location.href = "http://localhost:1337/home";
+}
 
-/* Sign up button */
+/* Sign up button 
 function clickFunction() {
     window.location.href = "http://localhost:1337/signup";
-}
+} */
