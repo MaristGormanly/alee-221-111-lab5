3b4e6ed94edb1b2a8d4edca1e510f4d781d6a28f
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-05-10 22:50:06 -0400

    userController

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -2,38 +2,44 @@ var user = require('../model/user');
 
 let users = [];
 
-let annie = user.createUser("Annie", "Lee", "annielee@gmail.com", "annielee", "alee", "annielee!");
-let george = user.createUser("George", "Washington", "georgewashington@gmail.com", "georgewashington", "gwashington", "georgewashington!");
-let abraham = user.createUser("Abraham", "Lincoln", "abrahamlincoln@gmail.com", "abrahamlincoln", "alincoln", "abrahamlincoln!");
-let benjamin = user.createUser("Benjamin", "Franklin", "benjaminfranklin@gmail.com", "benjaminfranklin", "bfranklin", "benjaminfranklin!");
+// list of users to choose from
+let annie = user.createUser("Annie", "Lee", "annielee@gmail.com", "alee", "annielee!");
+let george = user.createUser("George", "Washington", "georgewashington@gmail.com", "gwashington", "georgewashington!");
+let abraham = user.createUser("Abraham", "Lincoln", "abrahamlincoln@gmail.com", "alincoln", "abrahamlincoln!");
+let benjamin = user.createUser("Benjamin", "Franklin", "benjaminfranklin@gmail.com", "bfranklin", "benjaminfranklin!");
 users.push(annie);
 users.push(george);
 users.push(abraham);
 users.push(benjamin);
 
+// get mthod
 exports.getAllUsers = (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
 	res.send(users);
 }
 
+// get method
 exports.getUser = (req, res) => {
 	res.setHeader('Content-Type', 'application/json');
 	res.send(users[req.params.index]);
 }
 
+// post method
 exports.saveUser = (req, res) => {
 	let newUser = user.createUser(req.body.firstName, req.body.lastName, req.body.email, req.body.username, req.body.password);
-	users.push(newUser);
+	posts.push(newUser);
 	res.setHeader('Content-Type', 'application/json');
 	res.send(users);
 }
 
+// delete method
 exports.deleteUser = (req, res) => {
 	users.splice(req.params.userId, 1);
 	res.setHeader('Content-Type', 'application/json');
 	res.send(users);
 }
 
+// patch method
 exports.updateUser = (req, res) => {
 	var updatedUser = users[req.params.userId];
 	console.log(req.body.firstName);
@@ -52,6 +58,7 @@ exports.updateUser = (req, res) => {
 	res.send(users[req.params.userId]);
 }
 
+// put method
  exports.completeUpdateUser = (req, res) => {
 	users[req.params.userId] = user.createUser(req.body.firstName, req.body.lastName, req.body.email, req.body.username, req.body.password);
 	res.setHeader('Content-Type', 'application/json');
