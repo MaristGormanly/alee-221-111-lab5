bbe872fc2c32bb3753f7b7c3b13790f4171fdf97
Annie Lee <112659408+alee0724@users.noreply.github.com>
2023-05-10 23:46:21 -0400

    server.js

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json({type: 'application/json'}));
 
 var users = require('./controller/userController');
+var posts = require('./controller/socialMediaController');
 
 app.get('/', function (req, res) {
     res.sendFile('index.html', {root: './client/views'})
@@ -15,6 +16,10 @@ app.get('/signup', function (req, res) {
 
 app.get('/home', function (req, res) {
     res.sendFile('home.html', {root: './client/views'})
+})
+
+app.get('/createPost', function (req, res) {
+    res.sendFile('createPost.html', {root: './client/views'})
 })
 
 app.use(express.static('client/public'));
@@ -29,9 +34,23 @@ app.route('/api/users/:userId')
     .patch(users.updateUser)
     .put(users.completeUpdateUser)
 
+app.route('/api/posts')
+    .get(posts.getAllPosts)
+    .post(posts.savePost)
+
+app.route('/api/posts/:postId')
+    .get(posts.getPost)
+    .delete(posts.deletePost)
+    .patch(posts.updatePost)
+    .put(posts.completeUpdatePost)
+
 let userRoute = require('./route/userRoute');
+let socialMediaRoute = require('./route/socialMediaRoute');
 app.use('/api/user', userRoute);
+app.use('/api/post', socialMediaRoute);
 let userController = require('./controller/userController');
+let socialMediaController = require('./controller/socialMediaController');
 let userService = require('./service/userService');
+let socialMediaService = require('./service/socialMediaService');
 
 app.listen(1337, () => console.log('Social Media Login Page is now up!'));
